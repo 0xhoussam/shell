6dd68c3db0accdd3eb4eba0e298029074d88d1de
0xPride <32068899+0xPride@users.noreply.github.com>
2022-06-07 11:25:24 +0100

    refactoring

--- include/lexer.h
+++ include/lexer.h
@@ -15,93 +15,27 @@
 
 #include "libft.h"
 
-# define KEYWORDS "cd pwd unset echo export env exit"
-# define PUNCTUATIONS ", * ; ( ) | \" \' / { } [ ] | &"
-# define OPERATORS "+ - * / < > << >> = == != & && | ||"
+int prev_exit_code;
 
-enum e_token_type {
-	IDENTIFIER,
-	OPERATOR,
-	CONSTANTS,
-	KEYWORD,
-	LETERALS,
-	PUNCTUATION,
-	SPECIAL_CHARACTERS
-};
+typedef enum e_redir_type {
+  DOUBLE,
+  SINGLE,
+  HEREDOC
+} t_redir_type;
 
-enum e_keywords {
-	CD,
-	PWD,
-	//READ,
-	//SET,
-	UNSET, // doing...
-	ECHO, // done
-	//READONLY,
-	//SHIFT,
-	EXPORT,
-	ENV,
-	EXIT,
-	//IF, // done
-	//FI,
-	//ELSE,
-	//WHILE,
-	//DO,
-	//DONE,
-	//FOR,
-	//UNTIL,
-	//CASE,
-	//ESACK,
-	//BREAK,
-	//CONTINUE,
-	//EXIT,
-	//RETURN,
-	//TRAP,
-	//WAIT,
-	//EVAL,
-	//EXEC,
-	//ULIMIT,
-	//UMASK
-};
+typedef enum e_priority {
+  NONE,
+  AND,
+  OR
+} t_priority;
 
-enum e_punctuation {
-	CAMMA, // ,
-	ASTERISK, // *
-	SEMICOLON, // ;
-	OPEN_PARENTHESES, // (
-	CLOSED_PARENTHESES, // )
-	DOUBLE_QUOTAION, // "
-	SINGLE_QUOTAION, // '
-	SLASH, // /
-	OPEN_CURLY_BRACKET, // {
-	CLOSED_CURLY_BRACKET, // }
-	OPEN_SQUAR_BRACKETS, // [
-	CLOSED_SQUAR_BRACKETS, // ]
-	VERTICAL_BAR, // |
-};
-
-enum e_operators {
-	PLUS,
-	MINUS,
-	MULT,
-	DIVI,
-	LESS_THAT,
-	MORE_THAN,
-	DOUBLE_LESS_THAN,
-	DOUBLE_MORE_THAN,
-	AND,
-	DOUBLE_AND,
-};
-
-typedef struct s_token {
-	enum e_token_type	token_type;
-	enum e_punctuation	punc;
-	enum e_keywords		keyword;
-	enum e_operators	op;
-	char				*str;
-}	t_token;
-
-void	extract_token(const char *line, t_list **tokens, int *i, const char *word);
-t_list	*extract_tokens_from_line(const char *line);
-// void	echo_helper(const char *line, t_list **tokens, int *i);
-
+typedef struct s_cmd {
+  const char  *cmd_name;
+  t_list      *args;
+  const char  *in;
+  const char  *out;
+  const char  *deli;
+  t_redir_type redir;
+  t_priority  priority;
+} t_cmd;
 #endif
