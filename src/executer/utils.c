2ed39cd6a77a9527766ea2f50251b3eeba1ec5fd
oumoussa98 <abdelwahd.oumouss@gmail.com>
2022-06-14 18:07:01 +0100

    update(executer): utils.c add join args function

--- src/executer/utils.c
+++ src/executer/utils.c
@@ -6,7 +6,7 @@
 /*   By: aoumouss <aoumouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 21:51:35 by aoumouss          #+#    #+#             */
-/*   Updated: 2022/06/13 21:51:59 by aoumouss         ###   ########.fr       */
+/*   Updated: 2022/06/14 15:52:26 by aoumouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,29 @@ void close_pipe(int *pipe)
 		print_error("close pipe", USE_ERRNO);
 	if (close(pipe[0]))
 		print_error("close pipe", USE_ERRNO);
+}
+
+char **join_args(t_cmd *cmd)
+{
+	t_list *list;
+	char **args;
+	int list_size;
+	int i;
+
+	list = cmd->args;
+	if (!list)
+		return (NULL);
+	list_size = ft_lstsize(list);
+	args = ft_calloc(sizeof(char *), list_size + 1);
+	if (!args)
+		return (NULL);
+	i = 0;
+	while (list)
+	{
+		args[i] = (char *)list->content;
+		list = list->next;
+		i++;
+	}
+	args[i] = NULL;
+	return (args);
 }
