7eea45647a75c2a5fee0b689efec2504825ae90b
oumoussa98 <abdelwahd.oumouss@gmail.com>
2022-06-17 17:05:39 +0100

    Update(defs.h): update command not found exit code

--- include/defs.h
+++ include/defs.h
@@ -6,7 +6,7 @@
 /*   By: aoumouss <aoumouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 17:45:04 by aoumouss          #+#    #+#             */
-/*   Updated: 2022/06/14 17:51:37 by aoumouss         ###   ########.fr       */
+/*   Updated: 2022/06/17 11:08:30 by aoumouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@
 
 // ERRORS
 # define CMD_NOT_FOUND "command not found"
-# define CMD_NOT_FOUND_ERRNO -1
-# define EXECVE_FIALED "execve failed"
-# define EXECVE_FIALED_ERRNO 3
+# define CMD_NOT_FOUND_ERRNO 127
+# define EXECVE_FAILED "execve failed"
+# define EXECVE_FAILED_ERRNO 3
 # define PIPE_ERROR "pipe error"
 # define USE_ERRNO ""
 
