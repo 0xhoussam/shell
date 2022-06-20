495553b79396f1b6060f151835f22e0dadec4045
abdelouahed oumoussa <doonabdo98@gmail.com>
2022-06-20 17:42:28 +0000

    Update(cd): updated cd buitltin command

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,31 +3,79 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aoumouss <aoumouss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 15:37:39 by habouiba          #+#    #+#             */
-/*   Updated: 2022/06/14 17:57:19 by aoumouss         ###   ########.fr       */
+/*   Updated: 2022/06/20 15:24:35 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	open_home_dir(t_params *params);
+void	open_prev_dir(t_params *params);
+
 void	cd(t_params *params)
 {
-	char	**dir_name;
+	char	*dir_name;
+	char	*error;
 	DIR		*dir;
 
-	dir_name = join_args(params->cmd);
-	if (!dir_name)
-		return ;
-	dir = opendir(*dir_name);
+	dir_name = NULL;
+	if (ft_lstsize(params->cmd->args) > 1)
+		return (print_error("cd", "too many arguments"));
+	if (params->cmd->args)
+		dir_name = params->cmd->args->content;
+	if (!dir_name || !ft_strcmp(dir_name, "~"))
+		return (open_home_dir(params));
+	if (!ft_strcmp(dir_name, "-"))
+		return (open_prev_dir(params));
+	dir = opendir(dir_name);
 	if (!dir)
 	{
-		write(2, PROGRAM_NAME, ft_strlen(PROGRAM_NAME));
-		write(2, ": cd: ", ft_strlen(": cd: "));
-		perror(*dir_name);
+		error = ft_strjoin(dir_name, ": no such file or directory");
+		print_error("cd", error);
+	}
+	chdir(dir_name);
+	closedir(dir);
+}
+
+void	open_home_dir(t_params *params)
+{
+	char	*home;
+	char	*error;
+	DIR		*dir;
+
+	home = get_env_variable(params->env, "HOME");
+	if (!home)
+		return (print_error("cd", "HOME not set"));
+	dir = opendir(home);
+	if (!dir)
+	{
+		error = ft_strjoin(home, ": no such file or directory");
+		print_error("cd", error);
 		return ;
 	}
-	chdir(*dir_name);
+	chdir(home);
+	closedir(dir);
+}
+
+void	open_prev_dir(t_params *params)
+{
+	char	*old_pwd;
+	char	*error;
+	DIR		*dir;
+
+	old_pwd = get_env_variable(params->env, "OLDPWD");
+	if (!old_pwd)
+		return (print_error("cd", "OLDPWD not set"));
+	dir = opendir(old_pwd);
+	if (!dir)
+	{
+		error = ft_strjoin(old_pwd, ": no such file or directory");
+		print_error("cd", error);
+		return ;
+	}
+	chdir(old_pwd);
 	closedir(dir);
 }
