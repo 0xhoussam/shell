fc516d91dbcb49848f4f535680d1bd4d3e448226
0xPride <32068899+0xPride@users.noreply.github.com>
2022-06-23 16:09:26 +0100

    fix: fixing output field

--- src/parser/get-output-redir.c
+++ src/parser/get-output-redir.c
@@ -6,36 +6,36 @@
 /*   By: aoumouss <aoumouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 07:50:39 by habouiba          #+#    #+#             */
-/*   Updated: 2022/06/18 09:53:05 by habouiba         ###   ########.fr       */
+/*   Updated: 2022/06/23 16:06:16 by habouiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-int	get_output_redir(t_list **cmds, t_cmd *cmd, char *s, t_env_list *env)
+int get_output_redir(t_list **cmds, t_cmd *cmd, char *s, t_env_list *env)
 {
-	size_t	s_len;
-	size_t	i;
-	size_t	j;
+    size_t s_len;
+    size_t i;
+    size_t j;
 
-	if (!cmd || !s || !*s)
-		return (0);
-	s_len = ft_strlen(s);
-	if (s_len < 2)
-		return (0);
-	if (s[0] != '>')
-		return (0);
-	if (s[1] == '>')
-	{
-		cmd->out_redir = DOUBLE;
-		i = 2;
-	}
-	else
-	{
-		cmd->out_redir = SINGLE;
-		i = 1;
-	}
-	cmd->out = extract_word(s, &j, 1);
-	recursive_parser(cmds, cmd, &s[j], env);
-	return (1);
+    if (!cmd || !s || !*s)
+        return (0);
+    s_len = ft_strlen(s);
+    if (s_len < 2)
+        return (0);
+    if (s[0] != '>')
+        return (0);
+    if (s[1] == '>')
+    {
+        cmd->out_redir = DOUBLE;
+        i = 2;
+    }
+    else
+    {
+        cmd->out_redir = SINGLE;
+        i = 1;
+    }
+    cmd->out = extract_word(s, &j, i);
+    recursive_parser(cmds, cmd, &s[j], env);
+    return (1);
 }
