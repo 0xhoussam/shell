3078bb08eccca25b471e0cc1dc626a5077686508
0xPride <32068899+0xPride@users.noreply.github.com>
2022-06-23 14:12:45 +0100

    fix: removing space from end of cmd_name

--- src/parser/get-args.c
+++ src/parser/get-args.c
@@ -6,49 +6,55 @@
 /*   By: aoumouss <aoumouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 09:37:21 by habouiba          #+#    #+#             */
-/*   Updated: 2022/06/17 12:50:07 by habouiba         ###   ########.fr       */
+/*   Updated: 2022/06/23 14:11:32 by habouiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-size_t	get_args(t_cmd *cmd, const char *s)
+size_t get_args(t_cmd *cmd, const char *s)
 {
-	size_t	i;
-	char	*str;
+    size_t i;
+    char  *str;
 
-	i = 0;
-	if (!cmd || !cmd->cmd_name || !s || !*s)
-		return (0);
-	if (s[0] == '<' || s[0] == '>' || s[0] == '&' || s[0] == '|' || s[0] == '*'
-		|| s[0] == ';')
-		return (0);
-	if (s[0] == '"')
-	{
-		i++;
-		while (s[i] && s[i] != '"')
-			i++;
-		str = ft_substr(s, 0, i + 1);
-		ft_lstadd_back(&cmd->args, ft_lstnew(str));
-		return (i + 1);
-	}
-	if (s[0] == '\'')
-	{
-		i++;
-		while (s[i] && s[i] != '\'')
-			i++;
-		str = ft_substr(s, 0, i + 1);
-		ft_lstadd_back(&cmd->args, ft_lstnew(str));
-		return (i + 1);
-	}
-	else
-	{
-		while (s[i] && !ft_isspace(s[i]))
-		i++;
-	}
-	str = ft_substr(s, 0, i);
-	ft_lstadd_back(&cmd->args, ft_lstnew(str));
-	return (i);
+    i = 0;
+    if (!cmd || !cmd->cmd_name || !s || !*s)
+        return (0);
+    if (s[0] == '<' || s[0] == '>' || s[0] == '&' || s[0] == '|' ||
+        s[0] == '*' || s[0] == ';')
+        return (0);
+    if (s[0] == '"')
+    {
+        i++;
+        while (s[i] && s[i] != '"')
+            i++;
+        str = ft_substr(s, 0, i + 1);
+        ft_lstadd_back(&cmd->args, ft_lstnew(str));
+        return (i + 1);
+    }
+    if (s[0] == '\'')
+    {
+        i++;
+        while (s[i] && s[i] != '\'')
+            i++;
+        str = ft_substr(s, 0, i + 1);
+        ft_lstadd_back(&cmd->args, ft_lstnew(str));
+        return (i + 1);
+    }
+    else
+    {
+        while (s[i] && !ft_isspace(s[i]))
+            i++;
+    }
+    str = ft_substr(s, 0, i);
+    if (!*str)
+    {
+        free(str);
+        return (i);
+    }
+    else
+        ft_lstadd_back(&cmd->args, ft_lstnew(str));
+    return (i);
 }
 
 // size_t	get_args(t_cmd *cmd, const char *s)
--- src/parser/parser-utils.c
+++ src/parser/parser-utils.c
@@ -1,35 +1,35 @@
 #include "parser.h"
 
-char	*extract_word(const char *s, size_t *k, int start)
+char *extract_word(const char *s, size_t *k, int start)
 {
-	size_t	j;
-	size_t	i;
-	char	c;
+    size_t j;
+    size_t i;
+    char   c;
 
-	j = start;
-	while (s[j] && ft_isspace(s[j]))
-		j++;
-	i = j;
-	if (s[j] == '"')
-		c = '"';
-	else if (s[j] == '\'')
-		c = '\'';
-	else
-	{
-		while (s[j] && !ft_isspace(s[j]))
-			j++;
-		if (s[j])
-			*k = j + 1;
-		else
-			*k = j;
-		return (ft_substr(s, i, j - i + 1));
-	}
-	j++;
-	while (s[j] && s[j] != c)
-		j++;
-	if (s[j])
-			*k = j + 1;
-		else
-			*k = j;
-	return (ft_substr(s, i, j - i + 1));
+    j = start;
+    while (s[j] && ft_isspace(s[j]))
+        j++;
+    i = j;
+    if (s[j] == '"')
+        c = '"';
+    else if (s[j] == '\'')
+        c = '\'';
+    else
+    {
+        while (s[j] && !ft_isspace(s[j]))
+            j++;
+        if (s[j])
+            *k = j + 1;
+        else
+            *k = j;
+        return (ft_substr(s, i, j - i));
+    }
+    j++;
+    while (s[j] && s[j] != c)
+        j++;
+    if (s[j])
+        *k = j + 1;
+    else
+        *k = j;
+    return (ft_substr(s, i, j - i));
 }
