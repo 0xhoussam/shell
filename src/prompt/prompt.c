7c0647e9a8c76273d00effa6124bc164e407b9da
abdelouahed oumoussa <doonabdo98@gmail.com>
2022-06-27 22:08:53 +0000

    update(prompt): handle ctl+c and ctl+d

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,17 +6,35 @@
 /*   By: aoumouss <aoumouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 15:33:54 by marvin            #+#    #+#             */
-/*   Updated: 2022/06/27 15:42:38 by aoumouss         ###   ########.fr       */
+/*   Updated: 2022/06/27 19:09:13 by aoumouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*prompt(void)
+static void	int_handler(int sig);
+
+char *prompt(void)
 {
 	char	*line;
 
+	signal(SIGINT, int_handler);
+	signal(SIGQUIT, SIG_IGN);
 	line = readline(PROMPT);
+	if (!line)
+		exit(0);
 	add_history(line);
 	return (line);
+}
+
+static void int_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		kill(0, SIGQUIT);
+		ft_putchar_fd('\n', 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
