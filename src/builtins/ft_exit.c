abf7b0d0345d790112e738939e9b1ff3408f74ea
abdelouahed oumoussa <doonabdo98@gmail.com>
2022-07-03 16:37:37 +0000

    fix(ft_exit): fixed norm error

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: aoumouss <aoumouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:02:15 by aoumouss          #+#    #+#             */
-/*   Updated: 2022/07/02 22:09:04 by aoumouss         ###   ########.fr       */
+/*   Updated: 2022/07/03 17:29:02 by aoumouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ void	ft_exit(t_params *params)
 	cmd = params->cmd;
 	if (cmd->args->next && !is_number(cmd->args->next->content))
 	{
-			error = ft_strjoin("exit: ", cmd->args->next->content);
-			print_error_no_exit(error, "numeric argument required");
-			free(error);
-			g_exit_code = 2;
-			if (cmd->left_delimiter != PIPE)
-				exit(2);
-			return ;
+		error = ft_strjoin("exit: ", cmd->args->next->content);
+		print_error_no_exit(error, "numeric argument required");
+		free(error);
+		g_exit_code = 2;
+		if (cmd->left_delimiter != PIPE)
+			exit(2);
+		return ;
 	}
 	if (ft_lstsize(cmd->args) > 2)
 		return (print_error_no_exit("exit", "too many arguments"));
